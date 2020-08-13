3c12cd5fb15d52cc1046613b7a1c3eed8355103f
sidharthasuvarna <sidhartha.suvarna@gmail.com>
2020-08-13 15:23:59 -0700

    This is defect

--- src/practice.java
+++ src/practice.java
@@ -4,48 +4,10 @@ public class practice {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-	 int [] a = {1,2,3,4};
-	  
+	 String str1 = "hello";
 	 
-	 int sum = sumArray(a);
-	 
-	 //System.out.println(sum);
-	 
-	 int result = multiply(5,10);
-		System.out.println(result);
-	}
-
-	
-	
-	private static int multiply(int i, int j) {
-		// TODO Auto-generated method stub
+	 System.out.println(str1.toUpperCase());
 		
-		int k=1;
-		int sum=0;
-		while(k<=j)
-		{
-			sum = sum+i;
-			k++;
-		}
-		
-		return sum;
-		
-	
-	}
-
-
-
-	public static int sumArray(int[] a)
-	{
-		int sum =0;
-		
-		for(int i=0;i<a.length;i++)
-		{
-			//System.out.println(a[i]);
-			sum = sum + a[i];
-		}
-		
-		return sum;
 	}
 
 }
